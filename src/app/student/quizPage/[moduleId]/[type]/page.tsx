3f6a5d11994659b7afc369fee8c0c9fd478409b7
Chalana Prabhashwara <gmchalanaprabhashwara@gmail.com>
2025-06-04 12:39:28 +0530

    Refactor QuizPage component to improve type definition for PageParams

--- src/app/student/quizPage/[moduleId]/[type]/page.tsx
+++ src/app/student/quizPage/[moduleId]/[type]/page.tsx
@@ -3,12 +3,14 @@
 import { useRouter } from 'next/navigation';
 import QuizComponent from '../../../../components/QuizComponent';
 
-interface PageParams {
-  moduleId: string;
-  type: string;
-}
+type PageParams = {
+  params: { 
+    moduleId: string;
+    type: string;
+  };
+};
 
-export default function QuizPage({ params }: { params: PageParams }) {
+export default function QuizPage({ params }: PageParams) {
   const router = useRouter();
 
   const handleFinish = () => {
